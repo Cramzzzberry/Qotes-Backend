ea461736f58800163648a8729ddedc324a364d9a
Cramzzzberry <janroebantuan@gmail.com>
2023-12-01 11:13:35 +0800

    Added specific cors origins

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const http = require('http')
-const server = http.createServer(app)
 const { Server } = require('socket.io')
+
+const server = http.createServer(app)
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: {
+    origin: process.env.CORS_ORIGIN
+  }
 })
 const port = 3000
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
 //routes
 const userRoute = require('./routes/userRoute')
 const sheetsRoute = require('./routes/sheetsRoute')(io)
 const authRoute = require('./routes/authRoute')
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+  })
+)
 app.use(express.json())
 app.use('/users', userRoute)
 app.use('/sheets', sheetsRoute)
